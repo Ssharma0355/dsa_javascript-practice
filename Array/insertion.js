775e89b2e3d500e21ddd6a443afa7a016fcb3e31
sachin_sde <ssachin.s@myclnq.co.in>
2025-02-27 10:58:55 +0530

    inserttion

--- Array/insertion.js
+++ Array/insertion.js
@@ -2,13 +2,15 @@ let data = [12, 3, 22, 1, 44, 5, 33];
 let newEl = 69;
 let position = 3;
 console.log("Before Insertion",data)
-for(let i=data.length-1;i>=0;i--){ //starting array from 7 to 1
+for(let i=data.length-1;i>=0;i--){ 
+    //starting array from 7 to 1
     if(i>=position){
       //iterator is equal to position(3)
-      data[i + 1] = data[i]; // pushing element forward 12, 3, 22, 1,1, 44, 5, 33
+      data[i + 1] = data[i]; 
+      // pushing element forward [12, 3, 22, 1,1, 44, 5, 33]
       if (i == position) {
         //if iterator is exact equal to 3
-        data[i] = newEl; //inserting newEL on data[3] 12, 3, 22, 69,1, 44, 5, 33
+        data[i] = newEl; //inserting newEL=69 on data[3] [12, 3, 22, 69,1, 44, 5, 33]
       }
     }
 
