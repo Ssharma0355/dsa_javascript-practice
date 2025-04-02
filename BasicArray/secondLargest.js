e13599133b759b4084929b671439a4f2f739de51
sachin_sde <ssachin.s@myclnq.co.in>
2025-04-02 17:28:30 +0530

    secondLargest

---
+++ BasicArray/secondLargest.js
@@ -0,0 +1,10 @@
+function secondLargest(arr){
+    // let sortedArr = arr.sort((a,b)=>b-a) //sorting the array in descending order
+    // return sortedArr[1]; //returning the second element of the sorted array
+
+    // let soreteArr = arr.sort((a,b)=>a-b); //sorting the array in ascending order 
+    // return soreteArr;
+}
+
+const arr = [2,3,1,4,5];
+console.log(secondLargest(arr));
