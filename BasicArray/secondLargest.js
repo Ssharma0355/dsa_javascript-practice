c8ac4afd9067206e8263efe0944e633f1ad1ec9c
sachin_sde <ssachin.s@myclnq.co.in>
2025-04-03 07:47:31 +0530

    secondlargest

--- BasicArray/secondLargest.js
+++ BasicArray/secondLargest.js
@@ -1,10 +1,25 @@
-function secondLargest(arr){
-    // let sortedArr = arr.sort((a,b)=>b-a) //sorting the array in descending order
-    // return sortedArr[1]; //returning the second element of the sorted array
 
-    // let soreteArr = arr.sort((a,b)=>a-b); //sorting the array in ascending order 
-    // return soreteArr;
+
+function secondLargestNumber(arr){
+    let largest = arr[0];
+    let secondLargest =arr[0];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>largest){
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i]>secondLargest){
+            secondLargest = arr[i];
+        }
+    }
+    return secondLargest;
 }
+//2nd approach
+// function secondLargest(arr){
+//     let sortedArr = arr.sort((a,b)=>b-a) //sorting the array in descending order
+//     return sortedArr[1]; //returning the second element of the sorted array
+// }
 
 const arr = [2,3,1,4,5];
-console.log(secondLargest(arr));
+// console.log(secondLargest(arr));
+console.log(secondLargestNumber(arr));
